bedde00ba8a4caa64ca5a42cf0ca06d2e4b219d8
Tobias Bodewig <tbodewig@web.de>
2014-11-05 09:15:28 +0100

    CommandExecutor

--- QuestPlugin(AT)/src/de/philippTobias/questPlugin/QuestPlugin.java
+++ QuestPlugin(AT)/src/de/philippTobias/questPlugin/QuestPlugin.java
@@ -1,23 +1,18 @@
 package de.philippTobias.questPlugin;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class QuestPlugin extends JavaPlugin {
 
-	private CommandHandler commandHandler;
+	private QuestPluginCommangExecutor commandExecutor;
 
 	public void onEnable() {
-		commandHandler = new CommandHandler();
+		commandExecutor = new QuestPluginCommangExecutor(this);
+		// getCommand("basic").setExecutor(commandExecutor);
 		this.getLogger().info("Questplugin has been enabled.");
 	}
 
 	public void onDisable() {
 		this.getLogger().info("Questplugin has been disabled.");
-	}
-
-	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-		return commandHandler.onCommand(sender, cmd, commandLabel, args);
 	}
 }
--- QuestPlugin(AT)/src/de/philippTobias/questPlugin/QuestPluginCommangExecutor.java
+++ QuestPlugin(AT)/src/de/philippTobias/questPlugin/QuestPluginCommangExecutor.java
@@ -1,9 +1,16 @@
 package de.philippTobias.questPlugin;
 
 import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
-public class CommandHandler {
+public class QuestPluginCommangExecutor implements CommandExecutor {
+
+	private QuestPlugin plugin;
+
+	public QuestPluginCommangExecutor(QuestPlugin plugin) {
+		this.plugin = plugin;
+	}
 
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
 		if (cmd.getName().equalsIgnoreCase("basic")) {
