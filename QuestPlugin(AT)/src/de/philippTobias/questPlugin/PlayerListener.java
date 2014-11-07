00c9a84b37f9f69027989bc232d4e6099520164f
Tobias Bodewig <tbodewig@web.de>
2014-11-07 13:38:35 +0100

    Add Citizens 2

---
+++ QuestPlugin(AT)/src/de/philippTobias/questPlugin/PlayerListener.java
@@ -0,0 +1,20 @@
+package de.philippTobias.questPlugin;
+
+import net.citizensnpcs.api.event.NPCRightClickEvent;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerLoginEvent;
+
+public class PlayerListener implements Listener {
+
+	@EventHandler
+	public void onLogin(PlayerLoginEvent event) {
+
+	}
+
+	@EventHandler
+	public void onNPCRightClick(NPCRightClickEvent event) {
+
+	}
+}
--- QuestPlugin(AT)/src/de/philippTobias/questPlugin/QuestPlugin.java
+++ QuestPlugin(AT)/src/de/philippTobias/questPlugin/QuestPlugin.java
@@ -6,9 +6,13 @@ public class QuestPlugin extends JavaPlugin {
 
 	private QuestPluginCommangExecutor commandExecutor;
 
+	private PlayerListener playerListener;
+
 	public void onEnable() {
 		commandExecutor = new QuestPluginCommangExecutor(this);
 		// getCommand("basic").setExecutor(commandExecutor);
+		playerListener = new PlayerListener();
+		getServer().getPluginManager().registerEvents(playerListener, this);
 		this.getLogger().info("Questplugin has been enabled.");
 	}
 
